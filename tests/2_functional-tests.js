14e1dd3c6e25830ff61b918a4e7943d592332186
Haroun Ach <harounacho21@gmail.com>
2020-12-04 19:38:56 +0100

    Test View issues with multiple filters

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -98,4 +98,18 @@ suite("Functional Tests", function () {
         done();
       });
   });
+
+  // 6
+  test("View issues on a project with multiple filters", function (done) {
+    chai
+      .request(server)
+      .get("/api/issues/test")
+      .query({ created_by: "Haroun", open: false })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.isEmpty(res.body);
+        done();
+      });
+  });
 });
