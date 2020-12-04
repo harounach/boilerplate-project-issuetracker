5c194993384d0453c755af943bf748093e962714
Haroun Ach <harounacho21@gmail.com>
2020-12-04 14:54:33 +0100

    Post new issue

---
+++ dao/dao.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+let mongodb = require("mongodb");
+let mongoose = require("mongoose");
+let Issue;
+
+/** Connect to MongoDB */
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+/** Define Database Schema */
+let issueSchema = new mongoose.Schema(
+  // Schema definition
+  {
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    status_text: String,
+    open: { type: Boolean, required: true },
+    created_on: { type: Date, required: true },
+    updated_on: { type: Date, required: true },
+    project: String,
+  },
+  // options
+  {
+    collection: "issue_tracker",
+  }
+);
+
+/** Create Model */
+Issue = mongoose.model("Issue", issueSchema);
+
+module.exports = {
+  // Add new issue
+  createIssue: function (issueObjs, done) {
+    let newIssue = new Issue(issueObjs);
+    newIssue.save((err, data) => {
+      if (err) {
+        /* FAILURE */
+        console.error(err);
+        done(err);
+      } else {
+        /* SUCCESS */
+        done(null, data);
+      }
+    });
+  },
+
+  // Update an issue
+  updateIssue: function (filterObj, done) {
+    // TODO
+  },
+
+  // Delete an issue
+  deleteIssue: function (filterObj, done) {
+    // TODO
+  },
+
+  // Get all issues
+  getIssues: function (filterObj, done) {
+    // TODO
+  },
+};
--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,44 @@
-'use strict';
+"use strict";
+
+const IssueDAO = require("../dao/dao.js");
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
+    .get(function (req, res) {
+      let project = req.params.project;
+    })
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      let issueObj = {};
+      issueObj["issue_title"] = req.body.issue_title;
+      issueObj["issue_text"] = req.body.issue_text;
+      issueObj["created_by"] = req.body.created_by;
+      issueObj["assigned_to"] = req.body.assigned_to || "";
+      issueObj["status_text"] = req.body.status_text || "";
+      issueObj["open"] = true;
+      issueObj["created_on"] = new Date().toUTCString();
+      issueObj["updated_on"] = new Date().toUTCString();
+      issueObj["project"] = project;
+
+      // now insert the issue
+      IssueDAO.createIssue(issueObj, (err, data) => {
+        if (err) {
+          console.error(err);
+          res.json({ error: "Something went wrong" });
+        } else {
+          console.log(data);
+          res.json(data);
+        }
+      });
     })
-    
-    .post(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
     });
-    
 };
